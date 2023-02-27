26d1b9952d7b36aa3e2c858a969c2ff9859be151
aaloysius <aaloysius@gulfrelay.com>
2023-02-26 20:48:26 -0600

    Add project files.

---
+++ AzureBlobUpload/AzureBlobUpload/FileUpload.cs
@@ -0,0 +1,29 @@
+using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+namespace FileUploadFunction
+{
+    public static class FileUpload
+    {
+        [FunctionName("FileUpload")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req, ILogger log)
+        {
+            string Connection = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
+            string containerName = Environment.GetEnvironmentVariable("ContainerName");
+            Stream myBlob = new MemoryStream();
+            var file = req.Form.Files["File"];
+            myBlob = file.OpenReadStream();
+            var blobClient = new BlobContainerClient(Connection, containerName);
+            var blob = blobClient.GetBlobClient(file.FileName);
+            await blob.UploadAsync(myBlob);
+            return new OkObjectResult("file uploaded successfylly");
+        }
+    }
+}
---
+++ AzureBlobUpload/WebApplication1/Views/Home/Index.vbhtml
@@ -0,0 +1,12 @@
+﻿@Code
+    ViewData("Title") = "Home Page"
+End Code
+
+@{
+    ViewBag.Title = "Index";
+}
+<h3>Passing multiple parameter in URL</h3>
+@using (Html.BeginForm("index", "Home"))
+{
+<a href = "/home/MultipleParameter/?data1=678&data2=c-sharpcorner" > Click here for passing the parameter</a>
+}
